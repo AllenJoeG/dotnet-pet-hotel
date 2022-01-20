e6a7323826bba93fb7f55097250a2c1251fe4ff4
AllenJoeG <allen.joeg@gmail.com>
2022-01-20 15:17:01 -0600

    PetOwner and Pet models created. DbSet in ApplicationContext. Tables set up

---
+++ Migrations/20220120211327_CreatePetsTable2.0.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace dotnet_bakery.Migrations
+{
+    public partial class CreatePetsTable20 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pet",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PetBreed = table.Column<int>(type: "integer", nullable: false),
+                    PetColor = table.Column<int>(type: "integer", nullable: false),
+                    checkedInAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    petOwnerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pet", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Pet_PetOwner_petOwnerId",
+                        column: x => x.petOwnerId,
+                        principalTable: "PetOwner",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pet_petOwnerId",
+                table: "Pet",
+                column: "petOwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pet");
+        }
+    }
+}
--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,7 +5,43 @@ using System;
 
 namespace pet_hotel
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType 
+    {
+      Shepherd,
+      Poodle,
+      Beagle,
+      Bulldog,
+      Terrier,
+      Boxer,
+      Labrador,
+      Retriever
+    }
+    public enum PetColorType 
+    {
+      White, 
+      Black, 
+      Golden, 
+      Tricolor, 
+      Spotted
+    }
+    public class Pet 
+    {
+      public int id { get; set; }
+
+      [Required]
+      [JsonConverter(typeof(JsonStringEnumConverter))]
+      public PetBreedType PetBreed { get; set; }
+    
+
+      [Required]
+      [JsonConverter(typeof(JsonStringEnumConverter))]
+      public PetColorType PetColor {get; set; }
+
+      public DateTime? checkedInAt {get; set; }
+
+      [ForeignKey("PetOwner")]
+      public int petOwnerId { get; set; }
+
+      public PetOwner ownedBy { get; set; }
+    }
 }
